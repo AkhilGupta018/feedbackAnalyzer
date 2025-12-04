6738e79b3c59fe6bec5062008efe285c4daa5832
akhil <akgupta1337@gmail.com>
2025-12-04 17:17:59 +0530

    added env

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
 from typing import List
-from dotenv import load_dotenv
-
-# Load environment variables
-load_dotenv()
+import os
 
 
 # Define the output schema using Pydantic
@@ -51,8 +48,12 @@ prompt = PromptTemplate(
     partial_variables={"format_instructions": parser.get_format_instructions()},
 )
 
-# Initialize Gemini model
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.3)
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+# Use the key in LangChain
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash", temperature=0.3, api_key=GOOGLE_API_KEY
+)
 
 
 def analyze_feedback(feedback_text):
